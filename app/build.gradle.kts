7c8e114a8e764090eb398b75bd8ec7cc64931a40
Miquel Martí <miquelmarti111@gmail.com>
2024-09-12 13:05:10 +0200

    Migrate gradle build and settings files to kotlin script

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "net.cactii.mathdoku"
+
+    val minAndroidVersion = 25
+    val androidVersion = 35
+    val javaVersion = JavaVersion.VERSION_21
+
+    compileSdk = androidVersion
+    // https://developer.android.com/studio/releases/build-tools
+    buildToolsVersion = "34.0.0"
+
+    defaultConfig {
+        applicationId = namespace
+        minSdk = minAndroidVersion
+        targetSdk = androidVersion
+        versionCode = 600
+        versionName = "2.12"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        compileOptions.encoding = Charsets.UTF_8.name()
+
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
